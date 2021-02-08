0006b515b11a32ce5d1e6a473e5b5cc48e2de53d
João Marcelo <joaov7.1996@gmail.com>
2021-02-08 20:53:55 -0300

    Inserindo novos exemplos de uso dos builders de MacBook

--- main.go
+++ main.go
@@ -12,5 +12,17 @@ func main() {
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	fmt.Printf("%+v", macBookAir)
+	fmt.Printf("MacBookAir:\n%+v\n", macBookAir)
+
+	macBookPro13, err := factory.CreateNewMac(factory.MacBookPro13)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Printf("MacBookPro13:\n%+v\n", macBookPro13)
+
+	macBookPro16, err := factory.CreateNewMac(factory.MacBookPro16)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Printf("MacBookPro16:\n%+v\n", macBookPro16)
 }
